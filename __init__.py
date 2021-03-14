da8ae4c2ebaac844995cee395d67ce133d9de517
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-03-14 12:06:28 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+import subprocess
 
 
 class GamepadInit(MycroftSkill):
@@ -8,8 +9,12 @@ class GamepadInit(MycroftSkill):
     @intent_file_handler('init.gamepad.intent')
     def handle_init_gamepad(self, message):
         self.speak_dialog('init.gamepad')
+        #subprocess.call(["ROS command line command here. Picked up by listener"],shell=True)
 
-
+#what didn't work
+#subprocess.call(["sudo python3 Bluetooth.py"],shell=True) Not this easy. sudo commands are locked out by a certain default. 
+#The gamepad .py script can be initialized in the image on startup more easily. 
+        
 def create_skill():
     return GamepadInit()
 
